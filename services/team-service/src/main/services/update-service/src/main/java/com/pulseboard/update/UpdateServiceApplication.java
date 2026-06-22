4470e9314a37ae0f5867f6b73ab64e993ad1c68d
Suraj Agarwal <surajkumaragarwal36@gmail.com>
2026-06-22 15:44:33 +0530

    P0-09,P0-10,P0-11,P0-12: auth, team, update service skeletons running and registered in Eureka

---
+++ services/team-service/src/main/services/update-service/src/main/java/com/pulseboard/update/UpdateServiceApplication.java
@@ -0,0 +1,11 @@
+package com.pulseboard.update;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdateServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UpdateServiceApplication.class, args);
+    }
+}
